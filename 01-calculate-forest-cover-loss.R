a723a72c786da8e46702c5c217e59f88ea730223
vwmaus <victor.maus@wu.ac.at>
2022-02-09 17:13:05 +0100

    gee query working

--- 01-calculate-forest-cover-loss.R
+++ 01-calculate-forest-cover-loss.R
@@ -2,17 +2,20 @@ library(reticulate)
 library(geojsonio)
 library(stringr)
 library(rgee)
+library(rgeeExtra)
 library(dplyr)
 library(sf)
+library(progress)
+library(readr)
+library(tidyr)
 
 ee_check()
 ee_Initialize()
 
-v <- "20220203"
-
-global_quarry <- st_read(str_c("./data/global_mining_and_quarry_",v,".gpkg")) |> 
-  slice(1:10) |> 
-  sf_as_ee()
+# ------------------------------------------------------------------------------
+# set all local and gee input datasets 
+data_version <- "20220203"
+global_quarry_path <- str_c("./data/global_mining_and_quarry_",data_version,".gpkg")
 
 ecoregions <- ee$FeatureCollection("RESOLVE/ECOREGIONS/2017")
 
@@ -20,88 +23,152 @@ wcmc_areas <- ee$FeatureCollection("WCMC/WDPA/current/polygons")
 
 gfc <- ee$Image('UMD/hansen/global_forest_change_2020_v1_8')
 
-treecover <- gfc$select('treecover2000')
-treecover000 <- treecover$gt(0)
-treecover025 <- treecover$lte(25)
-treecover050 <- treecover$gt(25)$And(treecover$lte(50))
-treecover075 <- treecover$gt(50)$And(treecover$lte(75))
-treecover100 <- treecover$gt(75)
+# ------------------------------------------------------------------------------
+# prepare forest cover loss image 
+loss_area_image <- gfc$select('loss')$multiply(ee$Image$pixelArea())$addBands(gfc$select('lossyear'))
 
-loss_image <- gfc$select('loss')
-loss_area_image <- loss_image$multiply(ee$Image$pixelArea())
-loss_year <- gfc$select('lossyear')
+# ------------------------------------------------------------------------------
+# prepare tree cover masks for different initial forest cover shares
+treecover2000 <- gfc$select('treecover2000')
+treecover_mask <- treecover2000$updateMask(treecover2000$gt(0))$rename('forest_loss_000')$addBands(list(
+  treecover2000$updateMask(treecover2000$lte(25))$rename('forest_loss_025'),
+  treecover2000$updateMask(treecover2000$gt(25)$And(treecover2000$lte(50)))$rename('forest_loss_050'),
+  treecover2000$updateMask(treecover2000$gt(50)$And(treecover2000$lte(75)))$rename('forest_loss_075'),
+  treecover2000$updateMask(treecover2000$gt(75))$rename('forest_loss_100')))
 
-# Define a spatial filter as geometries that intersect
-spatialFilter <- ee$Filter$intersects(leftField = '.geo', rightField = '.geo', maxError = 10)
+# ------------------------------------------------------------------------------
+# prepare tree cover masks for protected areas and different initial forest cover shares
+# Not applicable, Not assigned, or Not reported -- these are aggregated and calculated by difference
+wcmc_types <- tribble(~id, ~description,
+                      'Ia',  'strict nature reserve',
+                      'Ib',  'wilderness area',
+                      'II',  'national park',
+                      'III', 'natural monument or feature',
+                      'IV',  'habitat/species management area',
+                      'V',   'protected landscape/seascape',
+                      'VI',  'PA with sustainable use of natural resources',
+                      'All', 'All types of protected areas')
 
-# Define a saveAll join
-distSaveAll = ee$Join$saveAll(matchesKey = 'ecoregion')
-
-# Add ecoregions as a property of mines
-global_quarry <- distSaveAll$apply(global_quarry, ecoregions, spatialFilter)$map(function(feat) {
-  eco <- ee$Feature(ee$List(feat$get('ecoregion'))$get(0))
-  ECO_ID <- eco$get('ECO_ID')
-  ECO_NAME <- eco$get('ECO_NAME')
-  BIOME_NAME <- eco$get('BIOME_NAME')
-  BIOME_NUM <- eco$get('BIOME_NUM')
-  properties <- feat$propertyNames()
-  selectProperties <- properties$filter(ee$Filter$neq('item', 'ecoregion'))
-  return(feat$select(selectProperties)$set(
-    'ecoregion_id', ECO_ID,
-    'ecoregion', ECO_NAME,
-    'biome_id', BIOME_NUM,
-    'biome', BIOME_NAME
-  ))
-})
-
-
-data <- global_quarry$map(function(feature) {
-  loss000 <- loss_area_image$mask(treecover000)$addBands(loss_year)$reduceRegion(
-    reducer = ee$Reducer$sum()$group({groupField = 1}),
-    geometry = feature$geometry(),
-    scale = 30,
-    tileScale = 2
-  )$rename(list("groups"), list("forest_loss_area_000"))
-  loss025 <- loss_area_image$mask(treecover025)$addBands(loss_year)$reduceRegion(
-    reducer = ee$Reducer$sum()$group({groupField = 1}),
-    geometry = feature$geometry(),
-    scale = 30,
-    tileScale = 2
-  )$rename(list("groups"), list("forest_loss_area_025"))
-  loss050 <- loss_area_image$mask(treecover050)$addBands(loss_year)$reduceRegion(
-    reducer = ee$Reducer$sum()$group({groupField = 1}),
-    geometry = feature$geometry(),
-    scale = 30,
-    tileScale = 2
-  )$rename(list("groups"), list("forest_loss_area_050"))
-  loss075 <- loss_area_image$mask(treecover075)$addBands(loss_year)$reduceRegion(
-    reducer = ee$Reducer$sum()$group({groupField = 1}),
-    geometry = feature$geometry(),
-    scale = 30,
-    tileScale = 2
-  )$rename(list("groups"), list("forest_loss_area_075"))
-  loss100 <- loss_area_image$mask(treecover100)$addBands(loss_year)$reduceRegion(
-    reducer = ee$Reducer$sum()$group({groupField = 1}),
-    geometry = feature$geometry(),
-    scale = 30,
-    tileScale = 2
-  )$rename(list("groups"), list("forest_loss_area_100"))
-  ee$Feature(
-        feature$geometry()$centroid(100),
-        loss000$combine({reducer2 = loss025})$combine({reducer3 = loss050})$combine({reducer4 = loss075})$combine({reducer3 = loss100})
-  )$copyProperties(feature)
-})
-
-ee_as_sf(data)
-
-ee_as_sf(global_quarry)
-
-# TODO: loop for each protected option
-for (p in c('Ia')) {
-  protected_areas <- wcmc_areas$filter(ee$Filter$inList('IUCN_CAT', p))
-  protcted_mask <- ee.Image.constant(1).clip(protected_areas).mask()
-  treecover000p <- treecover000$updateMask(protcted_mask)
+for (l in treecover_mask$bandNames()$getInfo()) {
+  tmp_mask <- treecover_mask$select(l)
+  for (p in wcmc_types$id){
+    wcmc_mask <- tmp_mask$updateMask(ee$Image$constant(1)$clip(wcmc_areas)$mask())
+    if(p != 'All'){
+      protected_areas <- wcmc_areas$filter(ee$Filter$inList('IUCN_CAT', list(p)))
+      wcmc_mask <- tmp_mask$updateMask(ee$Image$constant(1)$clip(protected_areas)$mask())
+    }
+    new_band <- str_c(l, '_', p)
+    treecover_mask <- treecover_mask$addBands(wcmc_mask$rename(new_band))
+  }
 }
 
-# TODO: Cluster commodities
+# ------------------------------------------------------------------------------
+# ecoregions spatial filter as geometries that intersect quarry and saveAll join
+spatialFilter <- ee$Filter$intersects(leftField = '.geo', rightField = '.geo', maxError = 10)
+distSaveAll = ee$Join$saveAll(matchesKey = 'ecoregion')
 
+# ------------------------------------------------------------------------------
+# define processing tiles
+# tiles <- rbind(c(-180,-90), c(180,-90), c(180,90), c(-180,90), c(-180,-90)) |> 
+#   list() |> 
+#   st_polygon() |> 
+#   st_sfc(crs = 'EPSG:4326') |> 
+#   st_make_grid(
+#     cellsize = c(5, 5),
+#     what = "polygons",
+#     square = TRUE,
+#     flat_topped = FALSE
+#   ) |> 
+#   st_as_sf() |> 
+#   mutate(tile_id = sapply(x, FUN = function(x){
+#     st_coordinates(x) |> 
+#       as_tibble() |>
+#       group_by(L1, L2) |> 
+#       summarise(X = min(X), Y = min(Y), .groups = 'drop') |> 
+#       transmute(tile_id = str_c(
+#         ifelse(X < 0, 
+#                str_c(str_pad(abs(X), width = 3, pad = '0'), 'W'), 
+#                str_c(str_pad(abs(X), width = 3, pad = '0'), 'E')),
+#         ifelse(Y < 0, 
+#                str_c(str_pad(abs(Y), width = 2, pad = '0'), 'S'), 
+#                str_c(str_pad(abs(Y), width = 2, pad = '0'), 'N'))
+#       )) |> 
+#       as.character()
+#   })) 
+
+# ------------------------------------------------------------------------------
+# define mining and quarry processing tiles
+n_tile_features <- 200
+global_quarry <- st_read(global_quarry_path, quiet = TRUE) |> 
+  filter(country != "Antarctica") |> # OSM includes some polygons in Antarctica: Removed 
+  mutate(tile_id = rep(1:n(), each = n_tile_features)[1:n()])
+  # st_join(y = tiles, left = TRUE, join = function(x, y, ...) st_intersects(st_centroid(st_geometry(x)), y, ...))
+
+# ------------------------------------------------------------------------------
+# calculate forest cover loss within mining and quarry per tile
+gee_dir <- str_c("./data/gee_tiles_forest_loss_", data_version)
+dir.create(gee_dir, showWarnings = FALSE, recursive = TRUE)
+pb <- progress_bar$new(total = length(unique(global_quarry$tile_id)))
+pb <- progress_bar$new(total = 3)
+for(tl in sort(unique(global_quarry$tile_id))[1:3]){
+  pb$tick()
+  
+  tile_feat <- filter(global_quarry, tile_id == tl)[1:100,]
+  
+  # Add ecoregions as a property of quarry and mining dataset
+  tile_feat <- distSaveAll$apply(sf_as_ee(tile_feat), ecoregions, spatialFilter)$map(function(feat) {
+    eco <- ee$Feature(ee$List(feat$get('ecoregion'))$get(0))
+    ECO_ID <- eco$get('ECO_ID')
+    ECO_NAME <- eco$get('ECO_NAME')
+    BIOME_NAME <- eco$get('BIOME_NAME')
+    BIOME_NUM <- eco$get('BIOME_NUM')
+    properties <- feat$propertyNames()
+    selectProperties <- properties$filter(ee$Filter$neq('item', 'ecoregion'))
+    return(feat$select(selectProperties)$set(
+      'ecoregion_id', ECO_ID,
+      'ecoregion', ECO_NAME,
+      'biome_id', BIOME_NUM,
+      'biome', BIOME_NAME
+    ))
+  })
+  
+  # compute forest loss area 
+  res <- tile_feat$map(function(feat) {
+    out_list <- lapply(treecover_mask$bandNames()$getInfo(), FUN = function(l){
+      loss_area_image$mask(treecover_mask$select(l))$reduceRegion(
+        reducer = ee$Reducer$sum()$group({groupField = 1}),
+        geometry = feat$geometry(),
+        scale = 30,
+        tileScale = 2
+      )$rename(list("groups"), list(l))
+    })
+    out_dic <- out_list[[1]]
+    for (i in 1:length(out_list)) {
+      out_dic <-out_dic$combine({reducer = out_list[[i]]})
+    }
+    ee$Feature(
+      feat$geometry()$centroid(100),
+      out_dic
+    )$copyProperties(feat)
+  }) |> 
+    ee_as_sf() |>
+    sf::st_drop_geometry()  |> 
+    tibble::as_tibble()
+  
+  # TODO: Clean groups...
+  ## lapply(list, function)
+  ## must save as RDF
+  res |> 
+    select(id, forest_loss = forest_loss_025) |> 
+    unnest(cols = forest_loss) |> 
+    mutate(year = 2000 + as.numeric(stringr::str_extract(forest_loss, pattern = "(?<=\"group\": ).*(?=,)")),
+           area = stringr::str_replace(forest_loss, " \\}", "END"),
+           area = as.numeric(stringr::str_extract(area, pattern = "(?<=\"sum\": ).*(?=END)")),
+           area = units::set_units(units::set_units(area, m^2), km^2)) |> 
+    dplyr::group_by(id) |> 
+    nest() |> 
+    write_rds(str_c(gee_dir, '/', str_pad(tl, width = 4, pad = "0"), '.rds'))
+  
+    dplyr::rename(!!paste0("area_", tree_cover) := area)
+}
+# TODO: Cluster commodities
