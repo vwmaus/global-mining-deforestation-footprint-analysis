9572c1408c6b8b0c026a2e5ca20b0b7bad4df520
vwmaus <vwmaus1@gmail.com>
2024-09-07 00:42:40 +0200

    Create data release including area rescaling

--- 05-remove-commodity-multi-counts.R
+++ 05-remove-commodity-multi-counts.R
@@ -13,37 +13,16 @@ gee_version_all <- "20240829a" # Maus et al. + OSM
 # Step 1: Load only the column names
 file_path <- str_c("./output/global_mining_and_quarry_forest_loss_", gee_version_all, ".csv")
 column_names <- names(read_csv(file_path, n_max = 0))
-
-# Step 2: Identify columns starting with "area_forest_loss_" and any other needed columns
-needed_columns <- column_names[starts_with("area_forest_loss_", vars = column_names)]
-additional_columns <- c("id", "id_hcluster", "isoa3", "country", "year", "list_of_commodities", "number_of_commodities")  # Add any other columns you need
-
-# Combine the columns
-columns_to_load <- c(additional_columns, needed_columns)
+columns_to_load <- c("id", "isoa3", "year", "list_of_commodities", column_names[starts_with("area_forest_loss_", vars = column_names)])
 
 # Step 3: Load only the selected columns
 mines_gee_all <- read_csv(file_path, col_types = cols_only(!!!setNames(rep("?", length(columns_to_load)), columns_to_load))) |>
     #filter(isoa3 %in% c("BRA")) |>
-    #transmute(id, id_hcluster, list_of_commodities, number_of_commodities, isoa3, country, year, area_forest_loss_000 = area_forest_loss_000) |>
+    #transmute(id, list_of_commodities, isoa3, year, area_forest_loss_000 = area_forest_loss_000) |>
     mutate(list_of_commodities = str_replace_all(list_of_commodities, "Alumina|Aluminum", "Bauxite")) |>
     mutate(list_of_commodities = ifelse(is.na((list_of_commodities)), "Unknown", list_of_commodities)) |>
-    group_by(id_hcluster) |>
-    mutate(list_of_commodities = list_of_commodities |> 
-           str_split(pattern = ",") |>                # Split each string into a list of items
-           unlist() |>                                # Flatten the list
-           unique() |>                                # Remove duplicates
-           sort() |>                                  # Sort alphabetically (optional)
-           paste(collapse = ",")) |>                  # Recombine into a comma-separated string
-    ungroup() |>
     mutate(number_of_commodities = str_count(list_of_commodities, ",") + 1) |>
-    relocate(number_of_commodities, .after = list_of_commodities) |>
-    group_by(isoa3, country, year) |>
-    mutate(across(starts_with("area_forest_loss"), ~ sum(.x), .names = "{.col}.total_country")) |>
-    group_by(year) |>
-    mutate(across(starts_with("area_forest_loss") & ! matches(".total"), ~ sum(.x), .names = "{.col}.total_global")) |>
-    ungroup() |>
-    mutate(across(starts_with("area_forest_loss") & ! matches(".total"), ~ .x / number_of_commodities, .names = "{.col}.adj_share"))
-
+    relocate(number_of_commodities, .after = list_of_commodities)
 gc()
 
 # Get list of commodities
@@ -54,20 +33,24 @@ unique_commodities <- mines_gee_all |>
   unique() |>
   sort() 
 
-# mines_gee_all |>
-#   reframe(across(starts_with("area_forest_loss") & ! matches(".adj_share"), sum),
-#           across(ends_with("adj_share"), ~ sum(.x * number_of_commodities))) |>
-#   pivot_longer(all_of(starts_with("area")))         
+mines_gee_all <- mines_gee_all |>
+    group_by(isoa3, year) |>
+    mutate(across(starts_with("area_forest_loss"), ~ sum(.x), .names = "{.col}.total_country")) |>
+    group_by(year) |>
+    mutate(across(starts_with("area_forest_loss") & ! matches(".total"), ~ sum(.x), .names = "{.col}.total_global")) |>
+    ungroup() |>
+    mutate(across(starts_with("area_forest_loss") & ! matches(".total"), ~ .x / number_of_commodities, .names = "{.col}.adj_share"))
+gc()
 
 #### Calculate deforestation per country and commodity
-def_country_comm <- lapply(unique_commodities, function(m) {
+mines_comm <- lapply(unique_commodities, function(m) {
   filter(mines_gee_all, str_detect(list_of_commodities, str_c("(^|,)", fixed(m), "(,|$)"))) |>
     mutate(material_name = m) |>
-    group_by(isoa3, country, material_name, year) |>
+    group_by(isoa3, material_name, year) |>
     reframe(id=paste(id, collapse = ","), across(starts_with("area_forest_loss") & !matches(".total"), sum), across(matches(".total"), unique))
 }) |>
   bind_rows() |>
-  group_by(isoa3, country, year) |>
+  group_by(isoa3, year) |>
   mutate(
     across(
       starts_with("area_forest_loss_") & !matches(".total|.adj"),
@@ -89,28 +72,28 @@ def_country_comm <- lapply(unique_commodities, function(m) {
     )) |>
     ungroup()
 
-# Check if total adjusted area equal/approximate the country mining area
-#def_country_comm <- read_csv(str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv"))
+write_csv(mines_comm, str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv"))
 
-def_country_comm |>
+# Check if total adjusted area equal/approximate the country mining area
+# mines_comm <- read_csv(str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv"))
+
+mines_comm |>
   ungroup() |>
   reframe(across(starts_with("area_forest_loss_000.") & !matches("total"), sum)) |>
   pivot_longer(all_of(starts_with("area")))
 
-def_country_comm |>
-  group_by(isoa3, country) |>
+mines_comm |>
+  group_by(isoa3) |>
   reframe(area_forest_loss_000.total_country = sum(unique(area_forest_loss_000.total_country))) |>
   reframe(area_forest_loss_000.total_country = sum(area_forest_loss_000.total_country))
 
-def_country_comm |>
+mines_comm |>
   group_by(year) |>
   reframe(area_forest_loss_000.total_global = sum(unique(area_forest_loss_000.total_global))) |>
   reframe(area_forest_loss_000.total_global = sum(area_forest_loss_000.total_global))
 
-write_csv(def_country_comm, str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv"))
-
 # Check commodities
-check_coal_gold <- def_country_comm |>
+check_coal_gold <- mines_comm |>
   ungroup() |>
   transmute(material_name, year, 
          area_multiple_counts = area_forest_loss_000 - area_forest_loss_025,
--- 06-create-data-release.R
+++ 06-create-data-release.R
@@ -11,20 +11,17 @@ dir.create(str_c("./output/data-release-", release_version), recursive = TRUE)
 # reade source files
 global_mining_forest_loss <- read_csv(str_c("./output/global_mining_and_quarry_forest_loss_", gee_version_all, ".csv"))
 
-global_mining_forest_loss <- read_csv("data/") 
-
 cluster_ids <- group_by(global_mining_forest_loss, id) |>
     reframe(id_hcluster = unique(id_hcluster), list_of_commodities = unique(list_of_commodities))
 
 global_mining_polygons <- st_read(str_c("data/mining-tree-cover-loss-",gee_version_all,"/mining_features_biomes_20240829a.geojson"))  |>
     left_join(cluster_ids, by = join_by("id")) |>
-    relocate(id_hcluster, .after = id) 
-    
-select(id, cluster_ids, isoa3, country, ecoregion, biome, area, geom = geometry)
+    select(id, id_hcluster, isoa3, country, area = mine_area, list_of_commodities, ecoregion, biome, geom = geometry)
 
-select(id, id_hcluster, list_of_commodities, year, all_of(starts_with("area_tree_cover_|area_forest_loss_")))
+global_mining_forest_loss <- select(global_mining_forest_loss, id, year, isoa3, starts_with("area_tree_cover_"), starts_with("area_forest_loss_"))
 
-global_mining_materials_forest_loss <- read_csv(str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv"))
+global_mining_materials_forest_loss <- read_csv(str_c("./output/global_commodity_forest_loss_", gee_version_all, ".csv")) |>
+    select(year, isoa3, material_name, ids = id, starts_with("area_forest_loss_"))
 
 # write release files
 write_csv(global_mining_materials_forest_loss, str_c("./output/data-release-",release_version,"/global_mining_materials_forest_loss.csv"))
@@ -32,4 +29,3 @@ write_csv(global_mining_materials_forest_loss, str_c("./output/data-release-",re
 write_csv(global_mining_forest_loss, str_c("./output/data-release-",release_version,"/global_mining_forest_loss.csv"))
 
 st_write(global_mining_polygons, str_c("./output/data-release-",release_version,"/global_mining_polygons.gpkg"), layer = "mining_polygons")
-
